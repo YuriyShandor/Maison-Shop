85044af6380f5e2fd1ff9c58127ba5bf1f49d45e
Yuriy Shandor <yuriyshandor@gmail.com>
2017-10-15 11:28:03 +0300

    Add Section Top-Slider(01)

--- dist/bandle.js
+++ dist/bandle.js
@@ -83,6 +83,23 @@ __webpack_require__(2);
 
 $(document).ready(function () {
   $('select').niceSelect();
+
+  $('.top-slider').on('init', function (event, slick) {
+    $('.top-slider__arrows .container').append('<div class="slider-count"><p><span id="current">1</span> of <span id="total">' + slick.slideCount + '</span></p></div>');
+  });
+  $('.top-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: $("#top-slider__next-arrow"),
+    prevArrow: $("#top-slider__prew-arrow")
+  });
+  $('.top-slider').on('afterChange', function (event, slick, currentSlide, nextSlide) {
+    $('.slider-count #current').html(currentSlide + 1);
+  });
+
+  $('.navbar-toggler-icon').on('click', function () {
+    $('.navbar-nav').toggleClass('navbar-nav_on-click');
+  });
 });
 
 /***/ }),
